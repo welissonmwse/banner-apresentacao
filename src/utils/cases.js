d59d80abb40a3bb21878489c057f58ce8d1f7850
Filipe Salles <filipe.salles@seatecnologia.com.br>
2025-04-04 16:44:27 -0300

    atualizando video pt3

--- src/utils/cases.js
+++ src/utils/cases.js
@@ -7,15 +7,15 @@ export const cases = [
     imgIndicador: "https://intranet.seatecnologia.com.br/documents/d/guest/banner_sesc",
     videoApresentacaoIa: "https://comercial.seatecnologia.com.br/sesc_avatar.webm",
     videoApresentacao: "https://comercial.seatecnologia.com.br/SESC.mp4"
-  },
+  },   
   {
     id: "2",
-    cliente: "Sesc",
+    cliente: "CLDF",
     logoClient: "https://intranet.seatecnologia.com.br/documents/d/guest/sesc_logo",
-    banner: "https://intranet.seatecnologia.com.br/documents/d/guest/mockup_sesc",
-    imgIndicador: "https://intranet.seatecnologia.com.br/documents/d/guest/banner_sesc",
-    videoApresentacaoIa: "https://comercial.seatecnologia.com.br/sesc_avatar.webm",
-    videoApresentacao: "https://comercial.seatecnologia.com.br/SESC.mp4"
+    banner: "https://www.seatecnologia.com.br/documents/d/guest/image-373",
+    imgIndicador: "https://www.seatecnologia.com.br/documents/d/guest/banner_cldf-1",
+    videoApresentacaoIa: "https://comercial.seatecnologia.com.br/videoApresentacaoIa_cldf.webm",
+    videoApresentacao: "https://comercial.seatecnologia.com.br/videoApresentacao_CLDF.mp4"
   },
   {
     id: "3",
